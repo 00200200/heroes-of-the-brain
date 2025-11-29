47855babea51e41ed86261a94f10e97805052cf3
marcelele-0 <mnem12321@gmail.com>
2025-11-29 19:04:54 +0100

    "feat: update BrainAccessData files and modify connector.py"

--- backend/src/connector.py
+++ backend/src/connector.py
@@ -1,5 +1,6 @@
 """ EEG measurement example
-Live CSV Snapshot: File contains ONLY the data from the last loop iteration.
+Infinite Loop: Runs until Ctrl+C is pressed.
+Saves CSV Snapshot every 1s.
 """
 
 import matplotlib.pyplot as plt
@@ -7,7 +8,7 @@ import matplotlib
 import time
 import os
 import numpy as np
-import threading
+import sys
 from scipy.signal import butter, sosfiltfilt
 
 from brainaccess.utils import acquisition
@@ -30,6 +31,7 @@ csv_filename = f'./BrainAccessData/{time.strftime("%Y%m%d_%H%M")}-snapshot.csv'
 header_str = "time," + ",".join(cap.values())
 
 print(f"Snapshot data will be saved to: {csv_filename}")
+print("Press Ctrl+C in the terminal to STOP the recording.")
 
 # --- Start ---
 with EEGManager() as mgr:
@@ -41,54 +43,67 @@ with EEGManager() as mgr:
     
     acquisition_start_time = time.time()
     
-    # Licznik: dokąd już przetworzyliśmy dane w pamięci
+    # Zmienne kontrolne
     last_idx = 0
-    
-    start_loop = time.time()
+    last_save_time = time.time()
+    save_interval = 1.0
     annotation = 1
 
-    # Pętla (10 sekund)
-    while time.time() - start_loop < 10:
-        time.sleep(1) # Czekamy sekundę, zbierają się nowe dane
-        
-        print(f"Sending annotation {annotation}")
-        eeg.annotate(str(annotation))
-        annotation += 1
-        
-        # Pobranie stanu bufora
-        eeg.get_mne()
-        
-        if eeg.data.mne_raw is not None:
-            d, t = eeg.data.mne_raw.get_data(return_times=True)
-            current_len = d.shape[1]
-            
-            # Jeśli przybyły nowe dane
-            if current_len > last_idx:
-                # Bierzemy TYLKO ten nowy kawałek (slice)
-                new_data = d[:, last_idx:]
-                new_times = t[last_idx:] + acquisition_start_time
-                
-                chunk_to_save = np.vstack((new_times, new_data)).T
-                
-                # 'w' - kasuje plik i wpisuje tylko ten nowy chunk
-                with open(csv_filename, 'w') as f:
-                    np.savetxt(f, chunk_to_save, delimiter=',', header=header_str, comments='')
-                
-                print(f"Overwritten CSV with {new_data.shape[1]} latest samples.")
-                
-                # Przesuwamy wskaźnik na koniec, żeby w następnej pętli nie brać tego samego
-                last_idx = current_len
+    try:
+        # --- NIESKOŃCZONA PĘTLA ---
+        while True:
+            current_time = time.time()
 
-    print("Stopping...")
+            # Timer: Co 1 sekundę
+            if current_time - last_save_time >= save_interval:
+                
+                print(f"--- 1s Tick: Annotation {annotation}")
+                try:
+                    eeg.annotate(str(annotation))
+                except:
+                    pass # Ignoruj błędy annotacji przy zrywaniu połączenia
+                annotation += 1
+                
+                # Update danych
+                eeg.get_mne()
+                
+                if eeg.data.mne_raw is not None:
+                    d, t = eeg.data.mne_raw.get_data(return_times=True)
+                    current_len = d.shape[1]
+                    
+                    if current_len > last_idx:
+                        new_data = d[:, last_idx:]
+                        new_times = t[last_idx:] + acquisition_start_time
+                        
+                        chunk_to_save = np.vstack((new_times, new_data)).T
+                        
+                        # Nadpisz plik
+                        with open(csv_filename, 'w') as f:
+                            np.savetxt(f, chunk_to_save, delimiter=',', header=header_str, comments='')
+                        
+                        print(f"   -> Overwritten CSV with {new_data.shape[1]} samples.")
+                        last_idx = current_len
+
+                last_save_time = current_time
+
+            # CPU chill
+            time.sleep(0.01)
+
+    except KeyboardInterrupt:
+        print("\n\n!!! STOPPING (Ctrl+C detected) !!!")
+
+    print("Closing connection...")
     eeg.stop_acquisition()
     mgr.disconnect()
 
-# --- Koniec ---
+# --- Koniec (zapisz FIF po przerwaniu) ---
+print("Saving final FIF file...")
 eeg.data.save(f'./BrainAccessData/{time.strftime("%Y%m%d_%H%M")}-raw.fif')
 eeg.close()
 
-# Wykres całości
+print("Plotting...")
 mne_raw = eeg.data.mne_raw
-mne_raw.apply_function(lambda x: x*10**-6)
-mne_raw.filter(1, 40).plot(scalings="auto", verbose=False)
-plt.show()
+if mne_raw is not None:
+    mne_raw.apply_function(lambda x: x*10**-6)
+    mne_raw.filter(1, 40).plot(scalings="auto", verbose=False)
+    plt.show()
